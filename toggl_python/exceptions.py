b4853602fd85059d5c9462e895a7af7af5c624d4
Ivlev Denis <di-erz@yandex.ru>
2020-06-25 16:19:56 +0300

    Add MethodNotAllowed exception

--- toggl_python/exceptions.py
+++ toggl_python/exceptions.py
@@ -10,8 +10,17 @@ class NotFound(Exception):
     pass
 
 
+class MethodNotAllowed(Exception):
+    pass
+
+
 class NotSupported(Exception):
     pass
 
 
-STATUS_2_EXCEPTION = {401: Unauthorized, 403: Forbidden, 404: NotFound}
+STATUS_2_EXCEPTION = {
+    401: Unauthorized,
+    403: Forbidden,
+    404: NotFound,
+    405: MethodNotAllowed,
+}
