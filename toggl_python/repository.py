be4b982c26d2ac39aa38ded368e9e432f7ca2ca1
Ivlev Denis <di-erz@yandex.ru>
2020-06-25 12:12:41 +0300

    Add additional nested methods support

--- toggl_python/repository.py
+++ toggl_python/repository.py
@@ -1,3 +1,7 @@
+from functools import partial
+
+import httpx
+
 from .api import Api
 from .entities import (
     BaseEntity,
@@ -17,18 +21,66 @@ from .entities import (
 
 class BaseRepository(Api):
     LIST_URL = ""
-    DETAIL_URL = LIST_URL + "/{id}"
+    DETAIL_URL = None
     ENTITY_CLASS = BaseEntity
+    ADDITIONAL_METHODS = {}
+
+    def __init__(self, base_url=None, auth=None):
+        super().__init__(base_url=base_url, auth=auth)
+        if not self.DETAIL_URL:
+            self.DETAIL_URL = self.LIST_URL + "/{id}"
+
+    def __getattr__(self, method: str):
+        try:
+            method = super().__getattr__(method)
+        except AttributeError:
+            if method in self.ADDITIONAL_METHODS.keys():
+                method = partial(
+                    self.additionat_method, **self.ADDITIONAL_METHODS[method]
+                )
+            else:
+                raise AttributeError(f"No such method ({method})!")
+        return method
+
+    def additionat_method(
+        self,
+        id: int,
+        url: str,
+        additional_id: int = None,
+        entity: object = None,
+        detail: bool = False,
+        params: dict = None,
+        data: dict = None,
+        files: dict = None,
+    ) -> httpx.Response:
+        """
+        Call additional method with specified url and params
+        """
+
+        if detail:
+            _url = (self.DETAIL_URL + "/" + url + "/{additional_id}").format(
+                id=id, additional_id=additional_id
+            )
+            return self._retrieve(_url, entity, headers=self.HEADERS)
+        else:
+            _url = (self.DETAIL_URL + "/" + url).format(id=id)
+            return self._list(_url, entity, headers=self.HEADERS)
+
+    def _retrieve(self, _url, entity_class, **kwargs):
+        response = self.get(_url)
+        return entity_class(**response.json())
 
     def retrieve(self, id: int = None, **kwargs):
         full_url = self.BASE_URL.join(self.DETAIL_URL.format(id=id))
-        response = self.get(full_url)
-        return self.ENTITY_CLASS(**response.json())
+        return self._retrieve(full_url, self.ENTITY_CLASS)
+
+    def _list(self, _url, entity_class, **kwargs):
+        response = self.get(_url)
+        return [entity_class(**entity) for entity in response.json()]
 
     def list(self, **kwargs):
         full_url = self.BASE_URL.join(self.LIST_URL)
-        response = self.get(full_url)
-        return [self.ENTITY_CLASS(**entity) for entity in response.json()]
+        return self._list(full_url, self.ENTITY_CLASS)
 
     def create(self, entity: ENTITY_CLASS):
         full_url = self.BASE_URL.join(self.LIST_URL)
