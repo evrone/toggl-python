6ce24220df1e83600f849bdbc085bd8c5685306f
a.vasilyev <a.vasilyev@evrone.com>
2020-08-27 13:42:01 +0300

    Removed response wrapper class.

--- toggl_python/response.py
+++
@@ -1,7 +0,0 @@
-class ListResponse(list):
-    def __init__(self, value, response):
-        super(ListResponse, self).__init__(value)
-
-        data = response.json()
-        self.total_count = data.get("total_count")
-        self.per_page = data.get("per_page")
