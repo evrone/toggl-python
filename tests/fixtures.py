588ffc4ad5a1d47b3f239c5ebec5d5551563eca0
a.vasilyev <a.vasilyev@evrone.com>
2020-08-28 11:46:36 +0300

    test repository pagination

---
+++ tests/fixtures.py
@@ -0,0 +1,47 @@
+REPORT_TIME_ENTRIES_RESPONSE = {
+    "total_grand": 0,
+    "total_billable": None,
+    "total_currencies": [{"currency": None, "amount": None}],
+    "total_count": 1,
+    "per_page": 50,
+    "data": [
+        {
+            "id": 45675345,
+            "pid": 44556545,
+            "tid": None,
+            "uid": 123456,
+            "description": "",
+            "start": "2020-08-24T15:43:11+03:00",
+            "end": "2020-08-24T15:43:19+03:00",
+            "updated": "2020-08-24T15:43:19+03:00",
+            "dur": 8000,
+            "user": "Test User",
+            "use_stop": True,
+            "client": "test-client",
+            "project": "test project",
+            "project_color": "0",
+            "project_hex_color": "#990099",
+            "task": None,
+            "billable": None,
+            "is_billable": False,
+            "cur": None,
+            "tags": [],
+        }
+    ],
+}
+
+TIME_ENTRIES_RESPONSE = [
+    {
+        "id": 45675345,
+        "guid": "3ab9166aac16cbf1374edda1cb652f69",
+        "wid": 4581172,
+        "pid": 44556545,
+        "billable": False,
+        "start": "2020-08-24T12:43:11+00:00",
+        "stop": "2020-08-24T12:43:19+00:00",
+        "duration": 8,
+        "duronly": False,
+        "at": "2020-08-24T12:43:19+00:00",
+        "uid": 123456,
+    },
+]
