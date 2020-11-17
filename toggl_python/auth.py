babdaf7280deb18f7a7fa57921c9fda3f34b4d35
Denis Ivlev <di-erz@yandex.ru>
2020-11-17 11:32:35 +0300

    Fix TokenAuth

--- toggl_python/auth.py
+++ toggl_python/auth.py
@@ -4,11 +4,11 @@ import httpx
 
 
 class BasicAuth(httpx.BasicAuth):
-    pass
+    """"""
 
 
 class TokenAuth(httpx.BasicAuth):
     SECRET: str = "api_token"
 
     def __init__(self, token: typing.Union[str, bytes]):
-        self.auth_header = self.build_auth_header(token, self.SECRET)
+        super().__init__(token, self.SECRET)
