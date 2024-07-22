41e7b692975c179fab50ce1cb7ff917b60cfa624
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-07-22 14:22:01 +0300

    Add User entity with logged endpoint

---
+++ toggl_python/entities/user.py
@@ -0,0 +1,14 @@
+from typing import Dict
+
+from toggl_python.api import ApiWrapper
+
+
+class CurrentUser(ApiWrapper):
+    prefix: str = "/me"
+
+    def logged(self) -> bool:
+        response = self.client.get(url=f"{self.prefix}/logged")
+        _ = response.raise_for_status()
+
+        # Returns 200 OK and empty response body
+        return response.is_success
