8273d10b28c6568408890fcdebe4a9ee35000a6f
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-07-23 11:52:16 +0300

    Minor fixes after pre-commit run on all files

--- toggl_python/api.py
+++ toggl_python/api.py
@@ -1,16 +1,19 @@
-from typing import TYPE_CHECKING, Dict, Final
+from __future__ import annotations
 
-from httpx import URL, Client, Headers
+from typing import TYPE_CHECKING
+
+from httpx import Client
+
 
 if TYPE_CHECKING:
     from toggl_python.auth import BasicAuth, TokenAuth
 
-COMMON_HEADERS: "Dict[str, str]" = {"content-type": "application/json"}
+COMMON_HEADERS: dict[str, str] = {"content-type": "application/json"}
 ROOT_URL: str = "https://api.track.toggl.com/api/v9"
 
 
 class ApiWrapper:
-    def __init__(self, auth: "BasicAuth | TokenAuth") -> None:
+    def __init__(self, auth: BasicAuth | TokenAuth) -> None:
         self.client = Client(
             base_url=ROOT_URL,
             auth=auth,
