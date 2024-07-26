52325b303cd0c50ad08a3f72692a21dcc310e684
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-07-26 11:11:26 +0300

    Allow to update CurrentUser

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,24 +1,33 @@
-from typing import TYPE_CHECKING
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Union
 
 import httpx
 import pytest
+from pydantic import ValidationError
 from toggl_python.auth import BasicAuth, TokenAuth
 from toggl_python.entities.user import CurrentUser
-from toggl_python.schemas.current_user import MeResponse, MeResponseWithRelatedData
+from toggl_python.exceptions import BadRequest
+from toggl_python.schemas.current_user import (
+    MeResponse,
+    MeResponseWithRelatedData,
+    UpdateMeResponse,
+)
 
-from tests.responses import (
+from tests.responses.me_get import (
     FAKE_TOKEN,
     ME_RESPONSE,
     ME_RESPONSE_SHORT,
     ME_RESPONSE_WITH_RELATED_DATA,
 )
+from tests.responses.me_put import UPDATE_ME_RESPONSE
 
 
 if TYPE_CHECKING:
     from respx import MockRouter
 
 
-def test_logged__ok(response_mock: "MockRouter") -> None:
+def test_logged__ok(response_mock: MockRouter) -> None:
     mocked_route = response_mock.get("/me/logged").mock(
         return_value=httpx.Response(status_code=200),
     )
@@ -31,7 +40,7 @@ def test_logged__ok(response_mock: "MockRouter") -> None:
     assert result is True
 
 
-def test_logged__exception_is_raised(response_mock: "MockRouter") -> None:
+def test_logged__exception_is_raised(response_mock: MockRouter) -> None:
     mocked_route = response_mock.get("/me/logged").mock(
         return_value=httpx.Response(status_code=403),
     )
@@ -44,7 +53,7 @@ def test_logged__exception_is_raised(response_mock: "MockRouter") -> None:
     assert mocked_route.called is True
 
 
-def test_me__ok(response_mock: "MockRouter") -> None:
+def test_me__ok(response_mock: MockRouter) -> None:
     mocked_route = response_mock.get("/me").mock(
         return_value=httpx.Response(status_code=200, json=ME_RESPONSE),
     )
@@ -78,7 +87,7 @@ def test_me__ok(response_mock: "MockRouter") -> None:
     assert result == expected_result
 
 
-def test_me__ok__with_empty_fields(response_mock: "MockRouter") -> None:
+def test_me__ok__with_empty_fields(response_mock: MockRouter) -> None:
     mocked_route = response_mock.get("/me").mock(
         return_value=httpx.Response(status_code=200, json=ME_RESPONSE_SHORT),
     )
@@ -109,7 +118,7 @@ def test_me__ok__with_empty_fields(response_mock: "MockRouter") -> None:
     assert result == expected_result
 
 
-def test_me__ok_with_related_data(response_mock: "MockRouter") -> None:
+def test_me__ok_with_related_data(response_mock: MockRouter) -> None:
     mocked_route = response_mock.get("/me").mock(
         return_value=httpx.Response(status_code=200, json=ME_RESPONSE_WITH_RELATED_DATA),
     )
@@ -146,3 +155,130 @@ def test_me__ok_with_related_data(response_mock: "MockRouter") -> None:
 
     assert mocked_route.called is True
     assert result == expected_result
+
+
+@pytest.mark.parametrize(
+    argnames=("field_name", "field_value"),
+    argvalues=[
+        ("beginning_of_week", 0),
+        ("country_id", 123),
+        ("default_workspace_id", 12345678),
+        ("email", "new_user@mail.com"),
+        ("fullname", "New User"),
+        ("timezone", "Europe/Moscow"),
+    ],
+    ids=(
+        "beginning_of_week",
+        "country_id",
+        "default_workspace_id",
+        "email",
+        "fullname",
+        "timezone",
+    ),
+)
+def test_update_me__ok(
+    response_mock: MockRouter,
+    authed_current_user: CurrentUser,
+    field_name: str,
+    field_value: Union[str, int],
+) -> None:
+    payload = {field_name: field_value}
+    fake_response = UPDATE_ME_RESPONSE.copy()
+    fake_response.update(**payload)
+    mocked_route = response_mock.put("/me").mock(
+        return_value=httpx.Response(status_code=200, json=fake_response),
+    )
+    expected_result = UpdateMeResponse.model_validate(fake_response)
+
+    result = authed_current_user.update_me(**payload)
+
+    assert mocked_route.called is True
+    assert result == expected_result
+
+
+def test_update_me__invalid_email(authed_current_user: CurrentUser) -> None:
+    """Raise default Pydantic ValidationError on invalid email.
+
+    Later, it will be wrapped into custom exception with clear error message.
+    """
+    error_message = "value is not a valid email address"
+
+    with pytest.raises(ValidationError, match=error_message):
+        _ = authed_current_user.update_me(email="invalid_mail@@mail.com")
+
+
+def test_update_me__email_is_already_used(
+    response_mock: MockRouter, authed_current_user: CurrentUser
+) -> None:
+    error_message = "user with this email already exists"
+    mocked_route = response_mock.put("/me").mock(
+        return_value=httpx.Response(status_code=400, text=error_message),
+    )
+
+    with pytest.raises(BadRequest, match=error_message):
+        _ = authed_current_user.update_me(email="existing_address@mail.com")
+
+    assert mocked_route.called is True
+
+
+@pytest.mark.parametrize(
+    argnames=("value"),
+    argvalues=["Canada", "Europe/Beerlin", "Materic/City"],
+    ids=("No city", "Typo", "Not existing timezone"),
+)
+def test_update_me__invalid_timezone(authed_current_user: CurrentUser, value: str) -> None:
+    error_message = f"Specified timezone {value} is invalid"
+
+    with pytest.raises(ValidationError, match=error_message):
+        _ = authed_current_user.update_me(timezone=value)
+
+
+def test_update_me__invalid_default_workspace_id(authed_current_user: CurrentUser) -> None:
+    error_message = "Input should be greater than or equal to 1"
+
+    with pytest.raises(ValidationError, match=error_message):
+        _ = authed_current_user.update_me(default_workspace_id=0)
+
+
+def test_update_me__unavailable_default_workspace_id(
+    response_mock: MockRouter, authed_current_user: CurrentUser
+) -> None:
+    """Raise if user is trying to set unavailable Workspace."""
+    error_message = "Invalid default_workspace_id"
+    mocked_route = response_mock.put("/me").mock(
+        return_value=httpx.Response(status_code=400, text=error_message),
+    )
+
+    with pytest.raises(BadRequest, match=error_message):
+        _ = authed_current_user.update_me(default_workspace_id=11111111)
+
+    assert mocked_route.called is True
+
+
+@pytest.mark.parametrize(
+    argnames=("value", "error_message"),
+    argvalues=[
+        (-1, "Input should be greater than or equal to 0"),
+        (7, "Input should be less than or equal to 6"),
+    ],
+    ids=("Negative", "More than max allowed value"),
+)
+def test_update_me__invalid_beginning_of_week(
+    authed_current_user: CurrentUser, value: int, error_message: str
+) -> None:
+    with pytest.raises(ValidationError, match=error_message):
+        _ = authed_current_user.update_me(beginning_of_week=value)
+
+
+def test_update_me__invalid_country_id(authed_current_user: CurrentUser) -> None:
+    error_message = "Input should be greater than or equal to 1"
+
+    with pytest.raises(ValidationError, match=error_message):
+        _ = authed_current_user.update_me(country_id=0)
+
+
+def test_update_me__invalid_fullname(authed_current_user: CurrentUser) -> None:
+    error_message = "String should have at least 1 character"
+
+    with pytest.raises(ValidationError, match=error_message):
+        _ = authed_current_user.update_me(fullname="")
--- toggl_python/entities/user.py
+++ toggl_python/entities/user.py
@@ -1,7 +1,21 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
+
+from httpx import HTTPStatusError
+
 from toggl_python.api import ApiWrapper
-from toggl_python.schemas.current_user import MeResponse, MeResponseWithRelatedData
+from toggl_python.exceptions import BadRequest
+from toggl_python.schemas.current_user import (
+    MeResponse,
+    MeResponseWithRelatedData,
+    UpdateMeRequest,
+    UpdateMeResponse,
+)
 
 
+if TYPE_CHECKING:
+    from pydantic import EmailStr
 class CurrentUser(ApiWrapper):
     prefix: str = "/me"
 
@@ -15,10 +29,45 @@ class CurrentUser(ApiWrapper):
     def me(self, with_related_data: bool = False) -> MeResponse:
         response_schema = MeResponseWithRelatedData if with_related_data else MeResponse
         response = self.client.get(
-            url=self.prefix, params={"with_related_data": with_related_data},
+            url=self.prefix,
+            params={"with_related_data": with_related_data},
         )
         _ = response.raise_for_status()
 
         response_body = response.json()
 
         return response_schema.model_validate(response_body)
+
+    def update_me(
+        self,
+        beginning_of_week: Optional[int] = None,
+        country_id: Optional[int] = None,
+        default_workspace_id: Optional[int] = None,
+        email: Optional[EmailStr] = None,
+        fullname: Optional[str] = None,
+        timezone: Optional[str] = None,
+    ) -> UpdateMeResponse:
+        """Update instance without validating if new value is equal to current one.
+
+        So API request will be sent anyway.
+        """
+        payload_schema = UpdateMeRequest(
+            beginning_of_week=beginning_of_week,
+            country_id=country_id,
+            default_workspace_id=default_workspace_id,
+            email=email,
+            fullname=fullname,
+            timezone=timezone,
+        )
+        payload = payload_schema.model_dump(mode="json", exclude_none=True, exclude_unset=True)
+
+        response = self.client.put(url=self.prefix, json=payload)
+
+        try:
+            _ = response.raise_for_status()
+        except HTTPStatusError as base_exception:
+            # Disable exception chaining to avoid huge not informative traceback
+            raise BadRequest(base_exception.response.text) from None
+
+        response_body = response.json()
+        return UpdateMeResponse.model_validate(response_body)
