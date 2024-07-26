85b51eadf8b7ddf9f5e977a15e0aa4e45deee279
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-07-26 11:28:52 +0300

    Replace explicit expected_result init with implicit usage of .model_validate
    It is better to check response example than writing tests with boilerplate code

--- tests/test_user.py
+++ tests/test_user.py
@@ -50,27 +50,7 @@ def test_me__ok(response_mock: MockRouter, authed_current_user: CurrentUser) -> 
     mocked_route = response_mock.get("/me").mock(
         return_value=httpx.Response(status_code=200, json=ME_RESPONSE),
     )
-    expected_result = MeResponse(
-        api_token=ME_RESPONSE["api_token"],
-        at=ME_RESPONSE["at"],
-        authorization_updated_at=ME_RESPONSE["authorization_updated_at"],
-        beginning_of_week=ME_RESPONSE["beginning_of_week"],
-        country_id=ME_RESPONSE["country_id"],
-        created_at=ME_RESPONSE["created_at"],
-        default_workspace_id=ME_RESPONSE["default_workspace_id"],
-        email=ME_RESPONSE["email"],
-        fullname=ME_RESPONSE["fullname"],
-        has_password=ME_RESPONSE["has_password"],
-        id=ME_RESPONSE["id"],
-        image_url=ME_RESPONSE["image_url"],
-        intercom_hash=ME_RESPONSE["intercom_hash"],
-        openid_email=ME_RESPONSE["openid_email"],
-        openid_enabled=ME_RESPONSE["openid_enabled"],
-        options=ME_RESPONSE["options"],
-        timezone=ME_RESPONSE["timezone"],
-        toggl_accounts_id=ME_RESPONSE["toggl_accounts_id"],
-        updated_at=ME_RESPONSE["updated_at"],
-    )
+    expected_result = MeResponse.model_validate(ME_RESPONSE)
 
     result = authed_current_user.me()
 
@@ -84,24 +64,7 @@ def test_me__ok__with_empty_fields(response_mock: MockRouter) -> None:
     )
     auth = BasicAuth(username="username", password="pass")  # noqa: S106
     user = CurrentUser(auth=auth)
-    expected_result = MeResponse(
-        at=ME_RESPONSE_SHORT["at"],
-        authorization_updated_at=ME_RESPONSE_SHORT["authorization_updated_at"],
-        beginning_of_week=ME_RESPONSE_SHORT["beginning_of_week"],  # limit to 1-7
-        country_id=ME_RESPONSE_SHORT["country_id"],
-        created_at=ME_RESPONSE_SHORT["created_at"],
-        default_workspace_id=ME_RESPONSE_SHORT["default_workspace_id"],
-        email=ME_RESPONSE_SHORT["email"],
-        fullname=ME_RESPONSE_SHORT["fullname"],
-        has_password=ME_RESPONSE_SHORT["has_password"],
-        id=ME_RESPONSE_SHORT["id"],
-        image_url=ME_RESPONSE_SHORT["image_url"],
-        openid_email=ME_RESPONSE_SHORT["openid_email"],
-        openid_enabled=ME_RESPONSE_SHORT["openid_enabled"],
-        timezone=ME_RESPONSE_SHORT["timezone"],
-        toggl_accounts_id=ME_RESPONSE_SHORT["toggl_accounts_id"],
-        updated_at=ME_RESPONSE_SHORT["updated_at"],
-    )
+    expected_result = MeResponse.model_validate(ME_RESPONSE_SHORT)
 
     result = user.me()
 
@@ -115,32 +78,7 @@ def test_me__ok_with_related_data(
     mocked_route = response_mock.get("/me").mock(
         return_value=httpx.Response(status_code=200, json=ME_RESPONSE_WITH_RELATED_DATA),
     )
-    expected_result = MeResponseWithRelatedData(
-        api_token=ME_RESPONSE_WITH_RELATED_DATA["api_token"],
-        at=ME_RESPONSE_WITH_RELATED_DATA["at"],
-        authorization_updated_at=ME_RESPONSE_WITH_RELATED_DATA["authorization_updated_at"],
-        beginning_of_week=ME_RESPONSE_WITH_RELATED_DATA["beginning_of_week"],
-        clients=ME_RESPONSE_WITH_RELATED_DATA["clients"],
-        country_id=ME_RESPONSE_WITH_RELATED_DATA["country_id"],
-        created_at=ME_RESPONSE_WITH_RELATED_DATA["created_at"],
-        default_workspace_id=ME_RESPONSE_WITH_RELATED_DATA["default_workspace_id"],
-        email=ME_RESPONSE_WITH_RELATED_DATA["email"],
-        fullname=ME_RESPONSE_WITH_RELATED_DATA["fullname"],
-        has_password=ME_RESPONSE_WITH_RELATED_DATA["has_password"],
-        id=ME_RESPONSE_WITH_RELATED_DATA["id"],
-        image_url=ME_RESPONSE_WITH_RELATED_DATA["image_url"],
-        intercom_hash=ME_RESPONSE_WITH_RELATED_DATA["intercom_hash"],
-        openid_email=ME_RESPONSE_WITH_RELATED_DATA["openid_email"],
-        openid_enabled=ME_RESPONSE_WITH_RELATED_DATA["openid_enabled"],
-        options=ME_RESPONSE_WITH_RELATED_DATA["options"],
-        projects=ME_RESPONSE_WITH_RELATED_DATA["projects"],
-        tags=ME_RESPONSE_WITH_RELATED_DATA["tags"],
-        time_entries=ME_RESPONSE_WITH_RELATED_DATA["time_entries"],
-        timezone=ME_RESPONSE_WITH_RELATED_DATA["timezone"],
-        toggl_accounts_id=ME_RESPONSE_WITH_RELATED_DATA["toggl_accounts_id"],
-        updated_at=ME_RESPONSE_WITH_RELATED_DATA["updated_at"],
-        workspaces=ME_RESPONSE_WITH_RELATED_DATA["workspaces"],
-    )
+    expected_result = MeResponseWithRelatedData.model_validate(ME_RESPONSE_WITH_RELATED_DATA)
 
     result = authed_current_user.me(with_related_data=True)
 
