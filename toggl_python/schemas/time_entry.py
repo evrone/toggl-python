3dcee3ff1e33d55a35e23ce228b0b0d099a18cc5
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-09-03 10:55:43 +0300

    Allow to update, bulk edit, delete and stop TimeEntries (#62)

--- toggl_python/schemas/time_entry.py
+++ toggl_python/schemas/time_entry.py
@@ -1,11 +1,33 @@
 from __future__ import annotations
 
-from datetime import datetime, timedelta, timezone
-from typing import List, Optional
+from datetime import datetime
+from enum import Enum
+from typing import Dict, List, Optional, Union
 
-from pydantic import AwareDatetime, field_serializer, field_validator
+from pydantic import AwareDatetime, field_serializer, model_serializer
 
-from toggl_python.schemas.base import BaseSchema
+from toggl_python.schemas.base import BaseSchema, SinceParamSchemaMixin
+
+
+class BulkEditTimeEntriesOperations(str, Enum):
+    add = "add"
+    remove = "remove"
+    # Renamed to avoid using system keyword
+    change = "replace"
+
+
+class BulkEditTimeEntriesFieldNames(str, Enum):
+    billable = "billable"
+    description = "description"
+    duration = "duration"
+    project_id = "project_id"
+    shared_with_user_ids = "shared_with_user_ids"
+    start = "start"
+    stop = "stop"
+    tag_ids = "tag_ids"
+    tags = "tags"
+    task_id = "task_id"
+    user_id = "user_id"
 
 
 class MeTimeEntryResponseBase(BaseSchema):
@@ -35,35 +57,11 @@ class MeTimeEntryWithMetaResponse(MeTimeEntryResponse):
     user_name: str
 
 
-class MeTimeEntryQueryParams(BaseSchema):
+class MeTimeEntryQueryParams(SinceParamSchemaMixin, BaseSchema):
     meta: bool
-    since: Optional[AwareDatetime] = None
     before: Optional[AwareDatetime] = None
     start_date: Optional[AwareDatetime] = None
     end_date: Optional[AwareDatetime] = None
-
-    @field_validator("since")
-    @classmethod
-    def check_if_since_is_too_old(cls, value: Optional[datetime]) -> Optional[datetime]:
-        if not value:
-            return value
-
-        now = datetime.now(tz=timezone.utc)
-        three_months = timedelta(days=90)
-        utc_value = value.astimezone(tz=timezone.utc)
-
-        if now - three_months > utc_value:
-            error_message = "Since cannot be older than 3 months"
-            raise ValueError(error_message)
-
-        return value
-
-    @field_serializer("since", when_used="json")
-    def serialize_since(self, value: Optional[datetime]) -> Optional[int]:
-        if not value:
-            return value
-
-        return int(value.timestamp())
 
     @field_serializer("before", "start_date", "end_date", when_used="json")
     def serialize_datetimes(self, value: Optional[datetime]) -> Optional[str]:
@@ -88,3 +86,48 @@ class MeWebTimerResponse(BaseSchema):
     projects: Optional[List]
     tags: List
     time_entries: List[WebTimerTimeEntryResponse]
+
+
+class TimeEntryRequest(BaseSchema):
+    billable: Optional[bool]
+    description: Optional[str]
+    project_id: Optional[int]
+    tag_ids: Optional[List[int]]
+    task_id: Optional[int]
+    user_id: Optional[int]
+    duration: Optional[int]
+    start: Optional[AwareDatetime]
+    stop: Optional[AwareDatetime]
+    shared_with_user_ids: Optional[List[int]]
+    tags: Optional[List[str]]
+
+    @field_serializer("start", "stop", when_used="json")
+    def serialize_datetimes(self, value: Optional[datetime]) -> Optional[str]:
+        if not value:
+            return value
+
+        return value.isoformat()
+
+
+class BulkEditTimeEntriesOperation(BaseSchema):
+    operation: BulkEditTimeEntriesOperations
+    field_name: BulkEditTimeEntriesFieldNames
+    field_value: Union[bool, str, int, AwareDatetime, List[int], List[str]]
+
+    @model_serializer(when_used="json")
+    def serialize_schema(self) -> Dict:
+        return {
+            "op": self.operation,
+            "path": f"/{self.field_name}",
+            "value": self.field_value,
+        }
+
+
+class BulkEditTimeEntriesResponseFailure(BaseSchema):
+    id: int
+    message: str
+
+
+class BulkEditTimeEntriesResponse(BaseSchema):
+    success: List[int]
+    failure: List[BulkEditTimeEntriesResponseFailure]
